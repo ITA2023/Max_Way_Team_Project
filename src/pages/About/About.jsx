0d5567afd517a4637e4c7210964487754c61616e
Shoha_Coder <127945777+Shoha-Coder@users.noreply.github.com>
2023-09-08 12:00:53 +0500

    #fix #i18n #Max_Way_Team_Project

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -9,12 +9,8 @@ const About = () => {
     <div>
       <h1>{t("AboutHeading")}</h1>
       <img src={AboutImg} className={Style.About_img} alt="" />
-      <p>Kompaniya 2005 yilning fevral oyida Toshkent shahrida tashkil etilgan. Hozirda kompaniyaning Toshkent 
-        <br /> shahrida 18 ta filiali mavjud. 
-        <br />
-        Taomnoma asosan klаb sendvichlari, hot-doglar, burgerlar, lavashlar va donarlardan iborat. Bizning ustuvor <br /> yo'nalishlarimiz - ingredientlarning yangiligi va sifati, qo'shimchalarning xilma-xilligi, arzon narxlar va <br /> mehmonlarning takliflariga e'tibor <br />
-        Har kuni turli xil odamlar MaxWay-dan buyurtma berishadi. Biz esa tashrif buyuruvchilar sonini ham, filiallar <br /> sonini ham oshirishga harakat qilmoqdamiz. Har bir tayyorlangan taom bilan biz o'ziga xos retseptlarning <br /> tafsilotlarini aniqlaymiz va sizning sevimli brendingiz bo'lishda davom etish uchun narx va sifatning <br /> mukammal muvozanatini qidiramiz. <br />
-        Agar siz kutilmaganda biz tomondan yomon xizmat yoki past sifatli pishirilgan taomga duch kelsangiz, bizga <br /> <a href="https://t.me/maxwaymasterfood_bot" target='_blank' rel='noreferrer'>@maxwaymasterfood_bot</a> manziliga murojaat qiling. Biz ijobiy va salbiy fikr-mulohazalarni mamnuniyat bilan <br /> qabul qilamiz. Mehmonning shikoyati - bu sovg'a, bu tufayli bizda o'sish uchun maqsad bo’ladi.</p>
+      {/* <p>{t("About")}</p> */}
+      <p className={Style.p}>{t("About.0")} <br /> {t('About.1')} <br /> {t('About.2')} <br /> {t("About.3")} <br /> {t('About.4')} <br /> {t('About.6')} <br /> {t('About.7')} <br /> {t("About.8")} <br /> {t('About.9')} <br /> {t('About.10')} <br />{t("About.11")} <br />{t("About.12")}</p>
     </div>
   )
 }
